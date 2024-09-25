1871d9137186c25f6c71fd663a0ae739d9a9615d
ign-is <igna.dev.contact@gmail.com>
2024-09-24 22:49:21 -0300

    Add Experience update

--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -1,7 +1,14 @@
 import { Canvas } from "@react-three/fiber";
+
 import { workExperiences } from "../constants";
+import { Suspense, useState } from "react";
+import { OrbitControls } from "@react-three/drei";
+import CanvasLoader from "../components/CanvasLoader";
+import Developer from '../components/Developer.jsx';
 
 const Experience = () => {
+    const [animationName, setAnimationName] = useState('idle');
+
     return (
         <section className="c-space my-20">
             <div className="w-full text-white-600">
@@ -10,15 +17,41 @@ const Experience = () => {
                 <div className="work-container">
                     <div className="work-canvas">
                         <Canvas>
+                            <ambientLight intensity={7} />
+                            <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+                            <directionalLight position={[10, 10, 10]} intensity={1} />
+                            <OrbitControls enableZoom={false} maxPolarAngle={Math.PI / 2} />
 
+                            <Suspense fallback={<CanvasLoader />}>
+                            <Developer position-y={-3} scale={3} animationName={animationName} />
+                            </Suspense>
                         </Canvas>
                     </div>
 
                     <div className="work-content">
                         <div className="sm:py-10 py-5 sm:px-5 px-2.5">
-                            {workExperiences.map(({ id, name, pos, duration, title, animation }) => (
-                                <div key={id} className="work-content_container group">
-                                
+                            {workExperiences.map(({ id, name, pos, duration, title, animation, icon }) => (
+                                <div 
+                                    key={id}
+                                    onClick={() => setAnimationName(animation.toLowerCase())} 
+                                    onPointerOver={() => setAnimationName(animation.toLocaleLowerCase())}
+                                    onPointerOut={() => setAnimationName('idle')}
+                                    className="work-content_container group">
+                                    <div className="flex flex-col h-full justify-start items-center py-2">
+                                        <div className="work-content_logo">
+                                            <img className="w-full h-full" src={icon} alt="logo" />
+                                        </div>
+
+                                        <div className="work-content_bar" />
+                                    </div>
+
+                                    <div className="sm:p-5 px-2.5 py-5">
+                                        <p className="font-bold text-white-800">{name}</p>
+                                        <p className="text-sm mb-5">
+                                            {pos} -- <span>{duration}</span>
+                                        </p>
+                                        <p className="group-hover:text-white transition-all ease-in-out duration-500">{title}</p>
+                                    </div>
                                 </div>
                             ))}
                         </div>
