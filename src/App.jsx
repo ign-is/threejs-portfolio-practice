00f174dccb0426f2b4062e4d309289c0488f085e
ign-is <igna.dev.contact@gmail.com>
2024-09-28 22:37:31 -0300

    Add import all sections to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+import About from "./sections/About";
+import Hero from "./sections/Hero";
 import Navbar from "./sections/Navbar";
+import Projects from "./sections/Projects";
+import Clients from "./sections/Clients";
+import Contact from "./sections/Contact";
+import Footer from "./sections/Footer";
+import Experience from "./sections/Experience";
 
 const App = () => {
   return (
     <main className="max-w-7xl mx-auto">
       <Navbar />
+      <Hero />
+      <About />
+      <Projects />
+      <Clients />
+      <Experience />
+      <Contact />
+      <Footer />
     </main>
   )
 }
